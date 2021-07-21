74661e2af07fe96d9a544c884152d77d2d70a420
Ginés Collado <gines.collado@gmail.com>
2021-07-21 12:34:00 +0200

    inicial local

--- src/main/java/es/gincol/hola/controllers/HolaMuntoController.java
+++ src/main/java/es/gincol/hola/controllers/HolaMuntoController.java
@@ -9,10 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * @author ginescolladonavarro
- *
- */
 @RestController
 @RequestMapping("/hola-mundo")
 public class HolaMuntoController {
